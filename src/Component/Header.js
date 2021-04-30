cd69ef8244ef949968b18f1d427826d06317a871
azharul77 <suvromulla921@gmail.com>
2021-05-01 02:38:26 +0600

    simple react app

---
+++ src/Component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <div className="ui fixed menu">
+        <div className="ui center aligned container ">
+          <h2 style={{ color: "green", margin: "0 auto"}}>Contact Manager</h2>
+        </div>
+      </div>
+    );
+}
+export default Header
